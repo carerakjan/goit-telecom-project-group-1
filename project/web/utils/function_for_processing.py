d7912d470bdaa96aa3c81f113a7905002d685a36
Eduard Kruts <kruts.eduard@gmail.com>
2024-08-31 12:05:49 +0300

    added processing_input_data #19

--- project/web/utils/function_for_processing.py
+++ project/web/utils/function_for_processing.py
@@ -21,3 +21,21 @@ def processing_data(file_csv):
     return df_cleaned
 
 # processed_df = processing_data('project/data/internet_service_churn.csv')
+
+def processing_input_data(input_data):
+    df = input_data
+    df = df.drop(columns=['id'])
+    df = df.astype(float)
+    missing_values = df.isnull().sum() / len(df)
+    columns_to_drop = [column for column in df.columns if missing_values[column] > 0.5]
+    df_cleaned = df.drop(columns=columns_to_drop)
+    df_cleaned = df_cleaned.dropna(subset=['download_avg', 'upload_avg'])
+    if 'reamining_contract' in df_cleaned.columns:
+        df_cleaned.loc[:, 'reamining_contract'] = df_cleaned['reamining_contract'].fillna(0)
+    columns_to_remove = ['bill_avg', 'service_failure_count']
+    df_cleaned = df_cleaned.drop(columns=[col for col in columns_to_remove if col in df_cleaned.columns])
+    scaler = StandardScaler()
+    numerical_columns = df_cleaned.select_dtypes(include=['float64', 'int64']).columns
+    df_cleaned[numerical_columns] = scaler.fit_transform(df_cleaned[numerical_columns])   
+   
+    return df_cleaned
