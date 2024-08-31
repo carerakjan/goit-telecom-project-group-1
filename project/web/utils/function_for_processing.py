f32795db2ffe15252015b3c74e15dae48adfab6a
Bohdan Hryhorian <bohdan.hryhorian@playtech.com>
2024-09-01 00:17:25 +0300

    removed unused import

--- project/web/utils/function_for_processing.py
+++ project/web/utils/function_for_processing.py
@@ -1,10 +1,9 @@
 import pandas as pd
 from web.utils.scale_data import scale
-from web.utils.get_main_features import main_features
 
 
 def processing_input_data(input_data):
-    # Создаем DataFrame
+    # Створюємо DataFrame
     df = pd.DataFrame([input_data])
 
     print("Original DataFrame:", df.head())
