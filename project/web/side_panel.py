4c375139a6831006eee2b7c7ddfb7766a8905879
Hanna Tarasenko <maracheva.ganna@gmail.com>
2024-09-01 21:13:04 +0200

    доданий зразок файлу #52

--- project/web/side_panel.py
+++ project/web/side_panel.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 @st.dialog("Instruction", width="large")         
 def inst():
@@ -6,13 +7,24 @@ def inst():
       instructions = file.read()
       st.write(instructions)
 
+
+@st.dialog("Зразок файлу", width="large")         
+def example():
+    showcsv = pd.read_csv("project/data/users.csv")
+    st.header("Перевірте відповідність колонок та даних для свого файлу")
+    st.write(showcsv)
+
 def base():
     # Кнопка "Повернутися на головну"
     if st.sidebar.button("Повернутися на головну 🏠"):
         pass
-     # Кнопка "Повернутися на головну"
+    # Кнопка "Повернутися на головну"
     if st.sidebar.button("Інструкція користування 📄"):
         inst()
+    # Кнопка "Зразок файлу"
+    if st.sidebar.button("Тестовий файл 📄"):
+        example()
+    
 
     # Кнопка "Оновити"
     if st.sidebar.button("Оновити 🔄"):
